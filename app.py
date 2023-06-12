e1523c26dc3cec435626d289883309764aba2c9d
Fabrizzio <fabrizzio.vilchez.e@utec.edu.pe>
2023-06-12 15:29:45 -0500

    Upload basic server configuration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
@@ -7,8 +7,20 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
+
+@app.route('/', methods=['POST'])
+def consulta():
+    # Obtén la consulta SQL desde la solicitud POST
+    consulta_sql = request.json['consulta']
+    # Ejecuta la consulta en la base de datos
+    resultado = db.engine.execute(consulta_sql)
+    # Convierte el resultado en una lista de diccionarios
+    resultados_dict = [dict(row) for row in resultado]
+
+    print(resultados_dict)
+
+    return jsonify(resultados_dict)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
